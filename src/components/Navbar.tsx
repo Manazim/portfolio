7743cbeac756200c4edfac3ce3c9d6f36e1daf7b
3A:AM <97380455+Manazim@users.noreply.github.com>
2025-06-15 23:48:06 +0800

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-4 left-1/2 -translate-x-1/2 z-20 bg-black/60 text-white px-6 py-2 rounded-full shadow-lg flex gap-4">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/skills">Skills</Link>
+      <Link to="/experience">Experience</Link>
+      <Link to="/gallery">Gallery</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  )
+}
