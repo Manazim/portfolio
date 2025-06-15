0a555eb8fb65a881e97a6117c700c5d94c4d59c7
3A:AM <97380455+Manazim@users.noreply.github.com>
2025-06-15 23:39:03 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Html } from '@react-three/drei';
-import Scene from './components/Scene';
-import './App.css';
+// App.tsx
+import { Canvas } from '@react-three/fiber'
+import { Scene } from './components/Scene'
 
-const App: React.FC = () => {
+export default function App() {
   return (
-    <div className="app-container">
-      <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <Scene />
-        <OrbitControls enableZoom={false} />
-      </Canvas>
-      <Html fullscreen>
-        <div className="overlay">
-          <h1>My 3D Portfolio</h1>
-          <p>Interactive 3D experiences with React, Three.js & GSAP</p>
-        </div>
-      </Html>
-    </div>
-  );
-};
-
-export default App;
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Scene />
+    </Canvas>
+  )
+}
