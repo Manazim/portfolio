c2480847a4fbd62878c7c795ad0b497d9fef00fe
3A:AM <97380455+Manazim@users.noreply.github.com>
2025-06-15 23:38:46 +0800

    Update Scene.tsx

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,28 +1,19 @@
-import React, { useRef, useEffect } from 'react';
-import { Mesh } from 'three';
-import { useFrame } from '@react-three/fiber';
-import gsap from 'gsap';
+// Scene.tsx
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import * as THREE from 'three'
 
-const Scene: React.FC = () => {
-  const cubeRef = useRef<Mesh>(null!);
+export function Scene() {
+  const meshRef = useRef<THREE.Mesh>(null!)
 
-  // Rotate continuously
-  useFrame((_, delta) => {
-    cubeRef.current.rotation.x += delta * 0.5;
-    cubeRef.current.rotation.y += delta * 0.3;
-  });
-
-  // GSAP entrance animation
-  useEffect(() => {
-    gsap.from(cubeRef.current.scale, { x: 0, y: 0, z: 0, duration: 1, ease: 'back.out(1.7)' });
-  }, []);
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01
+  })
 
   return (
-    <mesh ref={cubeRef} position={[0, 0, 0]}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="#00adee" />
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      <meshStandardMaterial color="hotpink" />
     </mesh>
-  );
-};
-
-export default Scene;
+  )
+}
