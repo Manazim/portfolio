070eb6f16c0eb575312db53f704e3c28336e4462
3A:AM <97380455+Manazim@users.noreply.github.com>
2025-06-15 23:47:35 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-// App.tsx
 import { Canvas } from '@react-three/fiber'
-import { Scene } from './components/Scene'
+import { Suspense } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Scene from './components/Scene'
+import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
+import Experience from './pages/Experience'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
 
 export default function App() {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Scene />
-    </Canvas>
+    <Router>
+      <div className="w-screen h-screen overflow-hidden relative">
+        <Canvas className="absolute top-0 left-0 z-0">
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
+        </Canvas>
+
+        <div className="relative z-10">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
